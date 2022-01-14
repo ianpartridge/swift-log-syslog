a25c23cf23e6259624594aa6cfac417e3512d62d
Dmitriy Borovikov <dmitry@borovikov.name>
2022-01-14 21:46:33 +0300

    Add option & facility to SysLogHandler

--- Sources/LoggingSyslog/SyslogLogHandler.swift
+++ Sources/LoggingSyslog/SyslogLogHandler.swift
@@ -3,10 +3,72 @@ import Logging
 
 /// A `LogHandler` which logs to `syslog(3)`.
 public struct SyslogLogHandler: LogHandler {
+    public enum Facility: Int32 {
+        case auth
+        case authpriv
+        case cron
+        case daemon
+        case ftp
+        case kern
+        case local0
+        case local1
+        case local2
+        case local3
+        case local4
+        case local5
+        case local6
+        case local7
+        case lpr
+        case mail
+        case news
+        case syslog
+        case user
+        case uucp
 
+        public var rawValue: Int32 {
+            switch self {
+            case .auth: return LOG_AUTH
+            case .authpriv: return LOG_AUTHPRIV
+            case .cron: return LOG_CRON
+            case .daemon: return LOG_DAEMON
+            case .ftp: return LOG_FTP
+            case .kern: return LOG_KERN
+            case .local0: return LOG_LOCAL0
+            case .local1: return LOG_LOCAL1
+            case .local2: return LOG_LOCAL2
+            case .local3: return LOG_LOCAL3
+            case .local4: return LOG_LOCAL4
+            case .local5: return LOG_LOCAL5
+            case .local6: return LOG_LOCAL6
+            case .local7: return LOG_LOCAL7
+            case .lpr: return LOG_LPR
+            case .mail: return LOG_MAIL
+            case .news: return LOG_NEWS
+            case .syslog: return LOG_SYSLOG
+            case .user: return LOG_USER
+            case .uucp: return LOG_UUCP
+            }
+        }
+    }
+    
+    public struct Option: OptionSet {
+        public let rawValue: Int32
+        public init(rawValue: Int32) {
+            self.rawValue = rawValue
+        }
+        
+        public static let cons = Option(rawValue: LOG_CONS)
+        public static let ndelay = Option(rawValue: LOG_NDELAY)
+        public static let nowait = Option(rawValue: LOG_NOWAIT)
+        public static let odelay = Option(rawValue: LOG_ODELAY)
+        public static let perror = Option(rawValue: LOG_PERROR)
+        public static let pid = Option(rawValue: LOG_PID)
+    }
+    
     /// Create a `SyslogLogHandler`.
-    public init(label: String) {
+    public init(label: String, ident: String? = nil, facility: Facility = .user, option: Option = .odelay) {
         self.label = label
+        openlog(ident, option.rawValue, facility.rawValue);
     }
 
     public let label: String
